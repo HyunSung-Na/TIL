3db599f113b6f2fc6b898506434b36eda16c8c5b
HyunSung-Na <59365947+HyunSung-Na@users.noreply.github.com>
2020-08-13 23:35:30 +0900

    TDD book 13장

--- 자바/TDD/src/main/java/com/tdd1/demo/money/Expression.java
+++ 자바/TDD/src/main/java/com/tdd1/demo/money/Expression.java
@@ -1,4 +1,5 @@
 package com.tdd1.demo.money;
 
 public interface Expression {
+    Money reduce(String to);
 }
--- 자바/TDD/src/main/java/com/tdd1/demo/money/tddTest1.java
+++ 자바/TDD/src/main/java/com/tdd1/demo/money/tddTest1.java
@@ -46,12 +46,37 @@ public class tddTest1 {
     @DisplayName("드디어 더하기")
     @Test
     public void testSimpleAddition() {
-        Money sum = Money.dollar(5).plus(Money.dollar(5));
-        assertEquals(Money.dollar(10), sum);
         Money five = Money.dollar(5);
         Expression sum1 = five.plus(five);
         Bank bank = new Bank();
         Money reduced = bank.reduce(sum1, "USD");
         assertEquals(Money.dollar(10), reduced);
+    }
+
+    @DisplayName("진짜로 만들기")
+    @Test
+    public void testPlusReturnSum(){
+        Money five = Money.dollar(5);
+        Expression result = five.plus(five);
+        Sum sum = (Sum) result;
+        assertEquals(five, sum.augend);
+        assertEquals(five, sum.addend);
+    }
+
+    @DisplayName("진짜로 만들기 2")
+    @Test
+    public void testReduceSum() {
+        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(7), result);
+    }
+
+    @DisplayName("진짜로 만들기 3")
+    @Test
+    public void testReduceMoney() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
     }
 }
