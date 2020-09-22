2781ce22d5ea4d8ed059845dbd6b52c234896350
HyunSung-Na <59365947+HyunSung-Na@users.noreply.github.com>
2020-09-22 22:34:33 +0900

    Node bird cloneCoding / next Js, react 기본 세팅

---
+++ JavaScript/react-nodebird-cloneCoding/prepare/front/components/AppLayout.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from "next/link";
+import { } from 'antd';
+
+const AppLayout = ({children}) => {
+    return (
+        <div>
+            <div>
+                <Link href="/"><a>노드버드</a></Link>
+                <Link href="/profile"><a>프로필</a></Link>
+                <Link href="/signup"><a>회원가입</a></Link>
+            </div>
+            {children}
+        </div>
+    )
+};
+
+AppLayout.prototype = {
+    children: PropTypes.node.isRequired,
+};
+
+export default AppLayout;
