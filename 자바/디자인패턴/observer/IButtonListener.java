36a685b3b72da312e61ce141d518a13e81a54876
HyunSung-Na <oceana57@naver.com>
2021-06-11 22:26:48 +0900

    Observer patten

---
+++ 자바/디자인패턴/observer/IButtonListener.java
@@ -0,0 +1,5 @@
+package com.baekjun.demo.디자인패턴.observer;
+
+public interface IButtonListener {
+    void clickEvent(String event);
+}
---
+++ 자바/디자인패턴/observer/Observer.java
@@ -0,0 +1,20 @@
+package com.baekjun.demo.디자인패턴.observer;
+
+public class Observer {
+
+    public static void main(String[] args) {
+        Button button = new Button("버튼");
+
+        button.addListener(new IButtonListener() {
+            @Override
+            public void clickEvent(String event) {
+                System.out.println(event);
+            }
+        });
+
+        button.click("메시지 전달 : click1");
+        button.click("메시지 전달 : click2");
+        button.click("메시지 전달 : click3");
+        button.click("메시지 전달 : click4");
+    }
+}
